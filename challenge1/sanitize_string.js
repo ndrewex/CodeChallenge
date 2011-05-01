46e6d9758528d77a40abcdb1650ef8ecb16c3bf0
Andrew Yee <ndrewex@gmail.com>
2011-05-01 04:31:49 -0700

    add comments to challenge1 and change some variables.

--- challenge1/sanitize_string.js
+++ challenge1/sanitize_string.js
@@ -1,13 +1,16 @@
-var SanitizeStringModule = require('./node_modules/mysql_real_escape_string');
+var MySQLRealEscapeStringModule = require('./node_modules/mysql_real_escape_string');
 try
 {
-	var stringToSanitize = process.argv[2];
-	var strSanitized = SanitizeStringModule.mysql_real_escape_string(stringToSanitize);
-	if(strSanitized.length != 0)
+	// Getting user input from commandline 
+	var stringToClean = process.argv[2];
+	
+	// Cleaning the string by executing mysql_real_escape_string function 
+	var sanitizedString = MySQLRealEscapeStringModule.mysql_real_escape_string(stringToClean);
+	
+	if(sanitizedString.length != 0)
 	{	
-		console.log('Here is the unsanitize string: ' 
-			    + stringToSanitize);
-		console.log('Here is the sanitized string: ' + stringToSanitize);
+		console.log('Here is the unsanitize string: ' + stringToClean);
+		console.log('Here is the sanitized string: ' + sanitizedString);
 	}  
 }
 catch(err) 
