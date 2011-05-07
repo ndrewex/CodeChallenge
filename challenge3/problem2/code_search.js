7c3f27c8270291ffb1391c03e20f081085f46e88
Andrew Yee <ndrewex@gmail.com>
2011-05-07 14:42:57 -0700

    added challenge3/problem2: sample program

---
+++ challenge3/problem2/code_search.js
@@ -0,0 +1,26 @@
+var MyFunctionModule = require('./node_modules/myFunction');
+try
+{
+	// Getting user input from commandline 
+	var stringToClean = process.argv[2];		
+
+	// Sample hardcoded variable use for testing
+	// var stringToClean = "ABC123ABC123ABC123ABC123ABC123ABC123";
+
+	if(isNaN(stringToClean))
+	{	
+	    // Cleaning the string by executing mysql_real_escape_string function
+            var sanitizedString = MyFunctionModule.myFunction(stringToClean);
+	    
+            console.log('Input code : ' + stringToClean);
+	    console.log('Here is SQL Statement with sanitized string: '); 
+	    console.log('	' + sanitizedString);
+	}
+}
+catch(err) 
+{ 		
+	if(err.message == "Cannot read property \'length\' of undefined")
+	    console.log("Usage: code_search \"input code here\"");
+	else
+	    console.log(err.message);
+}
